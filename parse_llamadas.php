763f94d003536aa686cbf29153dcab7a4ec4aec7
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-17 11:37:49 -0300

    Añadir namespace CallCenter.\n Añadir archivo de configuracion inicial.\n

--- parse_llamadas.php
+++ parse_llamadas.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace CallCenter;
+
 require "./Log.php";
 require "./ParseCalls.php";
 require "./CallCenterCalls.php";
@@ -9,17 +12,24 @@ require "./Record.php";
 require "./ManualCalls/ManualRecord.php";
 require "./PredictiveCalls/PredictiveRecord.php";
 
-$log_predictive_call = new \CallCenter\Log('/var/log/microvoz/parseX4_llamadas_predictivas.log');
-$log_manual_call = new \CallCenter\Log('/var/log/microvoz/parseX4_llamadas_manuales.log');
+$ini_file = parse_ini_file('/etc/microvoz/sistema.ini', true);
 
-$db = new \CallCenter\ConectorDB('parse', 'Milicom.', 'OP');
+$log_predictive_call = new Log('/var/log/microvoz/parseX4_llamadas_predictivas.log');
+$log_manual_call = new Log('/var/log/microvoz/parseX4_llamadas_manuales.log');
 
-$record_predictive = new \CallCenter\PredictiveCalls\PredictiveRecord($db, 'asterisk.cc_predictivo');
-$predictive_call = new \CallCenter\PredictiveCalls\PredictiveCalls($record_predictive, $db, $log_predictive_call);
+$db = new ConectorDB(
+    'parseX',
+    $ini_file['DB']['password'],
+    $ini_file['DB']['database'],
+    $ini_file['DB']['host']
+);
 
-$record_manual = new \CallCenter\ManualCalls\ManualRecord($db, 'asterisk.cc_manual');
-$manual_call = new \CallCenter\ManualCalls\ManualCalls($record_manual, $db, $log_manual_call);
+$record_predictive = new PredictiveCalls\PredictiveRecord($db, 'asterisk.cc_predictivo');
+$predictive_call = new PredictiveCalls\PredictiveCalls($record_predictive, $db, $log_predictive_call);
 
-$parse = new \CallCenter\ParseCalls();
+$record_manual = new ManualCalls\ManualRecord($db, 'asterisk.cc_manual');
+$manual_call = new ManualCalls\ManualCalls($record_manual, $db, $log_manual_call);
+
+$parse = new ParseCalls();
 
 $parse->start($manual_call, $predictive_call);

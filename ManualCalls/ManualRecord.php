94fe6e7cbfbb91bc2342fcfe6f5619d4f3be161d
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-17 12:51:30 -0300

    Bugfix: Acomodar sentencia SQL para obtener los registros de llamadas manuales

--- ManualCalls/ManualRecord.php
+++ ManualCalls/ManualRecord.php
@@ -31,11 +31,11 @@ class ManualRecord extends Record
                        M.id_destinos_softswitch AS id_destinos_softswitch, M.id_agente AS id_agente,
                        M.interno AS interno, C.pin AS pin, I.pin AS pin_interno, M.channel AS channel,
                        M.fecha_ini AS fecha_inicio, M.fecha_fin AS fecha_fin, M.fechahora AS fecha_inicio_talking, 
-                       M.id_campania AS id_campania, M.id_tarea AS id_tarea, M.id_base AS id_base, 
+                       M.id_campania AS id_campania, M.idtarea AS id_tarea, M.id_base AS id_base, 
                        M.estado_softswitch AS estado_softswitch, M.formato_discado AS formato_discado 
                  FROM {$this->table} AS M
                  LEFT JOIN {$this->campaign_table} C ON C.id = M.id_campania
-                 LEFT JOIN OP.Internos I ON I.interno = (if (M.interno = 0, -1, M.interno)
+                 LEFT JOIN OP.Internos I ON I.interno = (if (M.interno = 0, -1, M.interno))
                  WHERE M.estado IN (-3, 3, -4, 4, -5, 5, -6, 6, -7, 7, -8, -9, 9, 10, 17, 15, 31)";
 
         $this->records =  new ArrayObject($this->db->query($sql));

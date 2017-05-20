693c710c8aa0d015a605ba2149c48efa08478c96
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-20 11:23:37 -0300

    Remover metodo closePreviousRunningProcesses y getPreviousRunningProcesses ya que fueron pasados a la clase KillProcess.\n Añadir InboundCalls como dependencia al metodo start.\n

--- ParseCalls.php
+++ ParseCalls.php
@@ -17,31 +17,19 @@ class ParseCalls
     public function __construct($delay)
     {
         $this->delay = $delay;
-        $this->closePreviousRunningProcesses();
+        KillProcess::kill('KILL', $this->process_name);
     }
 
-    public function start(ManualCalls\ManualCalls $manual_calls, PredictiveCalls\PredictiveCalls $predictive_calls)
-    {
+    public function start(
+        ManualCalls\ManualCalls $manual_calls,
+        PredictiveCalls\PredictiveCalls $predictive_calls,
+        InboundCalls\InboundCalls $inbound_calls
+    ) {
         while (true) {
             $manual_calls->parseCall();
             $predictive_calls->parseCall();
+            $inbound_calls->parseCall();
             sleep($this->delay);
         }
-    }
-
-    private function closePreviousRunningProcesses()
-    {
-        foreach ($this->getPreviousRunningProcesses() as $process_detail) {
-            $process_id = [];
-            if (preg_match("/\d+/", $process_detail, $process_id)) {
-                shell_exec("kill -KILL {$process_id[0]}");
-            }
-        }
-    }
-
-    private function getPreviousRunningProcesses()
-    {
-        $list_previous_running_processes = shell_exec("ps fax | grep {$this->process_name} | grep -v grep");
-        return explode("\n", $list_previous_running_processes);
     }
 }

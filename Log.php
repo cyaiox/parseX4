11ef9961fd62418e99636695ae5a23f7059e0e80
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-20 11:11:48 -0300

    Añadir $debug para ver output en primer plano en la clase Log.\n Añadir global log al parse.

--- Log.php
+++ Log.php
@@ -12,16 +12,23 @@ namespace CallCenter;
 class Log
 {
     protected $file;
+    protected $global_log = '/var/log/microvoz/parseX4.log';
+    protected $debug;
 
-    public function __construct($file)
+    public function __construct($file, $debug=false)
     {
         $this->file = $file;
+        $this->debug = $debug;
     }
 
     public function log($message, $tipo, $id='')
     {
         $log_message = Date("[Y-m-d H:i:s]") . "[{$tipo}][{$id}] $message\n";
         file_put_contents($this->file, $log_message, FILE_APPEND | LOCK_EX);
+        file_put_contents($this->global_log, $log_message, FILE_APPEND | LOCK_EX);
+        if ($this->debug) {
+            echo $log_message;
+        }
     }
 
 }

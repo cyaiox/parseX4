4cf718e7f0fe21992fd0136c7dd4e6282a58e179
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-20 11:13:45 -0300

    Añadir id_cc_integracion a la sentencia SQL.

--- PredictiveCalls/PredictiveRecord.php
+++ PredictiveCalls/PredictiveRecord.php
@@ -28,7 +28,8 @@ class PredictiveRecord extends Record
         $sql = "SELECT id, idtarea AS id_tarea, id_base, idcampania AS extendido, subagrupacion, telefono, 
                        interno, estado, agente, id_agente, base, id_campania, pin, id_grabaciones,
 					   fecha_fin, fecha_inicio_talking, fecha_inicio, id_canal, id_destinos_softswitch, 
-					   id_agendados_discador AS id_agendado, estado_softswitch, formato_discado
+					   id_agendados_discador AS id_agendado, estado_softswitch, formato_discado, 
+					   id_cc_integracion AS id_gestion
                 FROM {$this->table}
                 WHERE estado IN (3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)";
 
@@ -39,13 +40,9 @@ class PredictiveRecord extends Record
 
     public function deleteRecord()
     {
-        if ($this->id)
-        {
-            $sql = "DELETE FROM {$this->table} WHERE id = '{$this->id}' AND telefono = '{$this->telefono}'";
-            return $this->db->query($sql);
-        }
+        $sql = "DELETE FROM {$this->table} WHERE id = '{$this->id}' AND telefono = '{$this->telefono}'";
 
-        return false;
+        return $this->db->query($sql);
     }
 
     public function getBill()

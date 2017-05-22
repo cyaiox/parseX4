5a3ec2a3ec5015fccaa5e9514116cb959e92355f
Gabriel diaz <gabrieldiaz31@gmail.com>
2017-05-22 12:16:47 -0300

    Añador log al metodo joinLlamadoGestion de llamadas de integracion.\n

--- IntegrationCalls/IntegrationCalls.php
+++ IntegrationCalls/IntegrationCalls.php
@@ -52,11 +52,39 @@ class IntegrationCalls extends CallCenterCalls
 
     public function joinLlamadoGestion($id_gestion, $id_padre=0, $id_hijo=0)
     {
+        $this->log->log(
+            "Preparacion para insertar en OP.comunicaciones_gestiones",
+            $this->record->getTipo(),
+            $this->record->getID()
+        );
+
         $sql = "INSERT INTO OP.comunicaciones_gestiones (comunicaciones_tabla, id_comunicacion, id_gestion) 
                 SELECT comunicaciones_tabla, id_comunicacion, {$id_gestion}
                 FROM OP.comunicaciones_gestiones
                 WHERE id_gestion = " . ($id_padre ? $id_padre : $id_hijo);
 
-        return $this->db->query($sql);
+        $this->log->log(
+            "SQL a realizar [{$sql}]",
+            $this->record->getTipo(),
+            $this->record->getID()
+        );
+
+        if ($this->db->query($sql)) {
+            $this->log->log(
+                "Insert realizado satisfactoriamente",
+                $this->record->getTipo(),
+                $this->record->getID()
+            );
+
+            return true;
+        } else {
+            $this->log->log(
+                "Error en la insercion con id_gestion: [{$id_gestion}]",
+                $this->record->getTipo(),
+                $this->record->getID()
+            );
+        }
+
+        return false;
     }
 }
